64e93b36aba432d004659d4a2aa768a2e51d2b28
Colosdubonheur <contact@colosdubonheur.fr>
2026-02-08 02:30:40 +0100

    Séparer les données runtime personnages du code versionné

--- api/get.php
+++ api/get.php
@@ -1,1 +1,4 @@
-<?php header('Content-Type: application/json'); echo file_get_contents('../data/personnages.json');
+<?php
+header('Content-Type: application/json; charset=utf-8');
+require_once __DIR__ . '/_data_store.php';
+echo file_get_contents(cluedo_data_path());
